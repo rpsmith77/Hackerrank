d6b52724415f8c0c736b537e79f09c47aa6def5d
Rpsmith77 <rpsmith77@gmail.com>
2019-09-12 20:56:34 -0400

    Inital Commit

---
+++ Loops.py
@@ -0,0 +1,15 @@
+# Task
+#   Read an integer N . For all non-negative integers i < N , print i^2.
+#
+# Input Format
+#   The first and only line contains the integer, N.
+
+# Output Format
+#   Print N lines, one corresponding to each i.
+
+
+n = int(input())
+i = 0
+while(i < n):
+    print(i ** 2)
+    i = i + 1
