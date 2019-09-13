eef8fc421d7b0f6f04b1e07d724cdb449513d464
Rpsmith77 <rpsmith77@gmail.com>
2019-09-12 21:03:25 -0400

    Inital Commit

---
+++ Loops Multiples of 5.py
@@ -0,0 +1,7 @@
+# Write a code segment using a FOR loop that prints multiples of 5 from 5 to 500, one on a line.
+#
+# Output Format
+#   One number per line. Include 5 and 500.
+
+for n in range(5, 505, 5):
+    print(n)
